70d2f9300594aff94b92063ca8380436e64bc6ec
mirkoperamas <1421133@utp.edu.pe>
2021-09-29 19:15:47 -0500

    actualizacion 29-09_19:15

--- src/app/components/inicio/inicio.component.ts
+++ src/app/components/inicio/inicio.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-inicio',
   templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css','../general-style-components.css'],
+  styleUrls: ['./inicio.component.css','../general-style-components.css']
 })
 export class InicioComponent implements OnInit {
 
--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
+  styleUrls: ['./navigation.component.css','../general-style-components.css']
 })
 export class NavigationComponent implements OnInit {
 
